cdcf3199530545bd97db285b3ca7afe72738f0b6
beomseon <beomsun22@gmail.com>
2022-02-11 10:49:37 +0900

    init

---
+++ src/test/java/com/bs/hellogitflow/HelloGitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.hellogitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGitFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
