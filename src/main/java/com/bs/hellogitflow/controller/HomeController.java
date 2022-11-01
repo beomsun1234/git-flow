1a3a7969fcabff67b19a6c8587c164a9824d3cdd
beomseon <beomsun22@gmail.com>
2022-11-01 17:33:52 +0900

    [#4] issue test helloservice

--- src/main/java/com/bs/hellogitflow/controller/HomeController.java
+++ src/main/java/com/bs/hellogitflow/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.bs.hellogitflow.controller;
 
+import com.bs.hellogitflow.service.HelloService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
-
+    private final HelloService helloService;
     @GetMapping("/")
     public String home(){
         return "home 1";
     }
 
+    @GetMapping("/hello")
+    public String sayHello(){
+        return helloService.sayHello("park");
+    }
 }
